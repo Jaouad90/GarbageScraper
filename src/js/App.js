ef4956067a7fb1e5f620bd3ff15fac295a90b766
Jaouad90 <jaouad@sendsteps.com>
2020-02-14 09:57:32 +0100

    workingState

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,4 @@
+
 function init() {
   // doSomething();
   getFillLevels();
@@ -8,17 +9,17 @@ function apiCall(Endpoint, DateFilter) {
   console.log(Url);
   try {
     fetch(Url)
-    .then(response => {
-      if (!response.ok)   {
-        throw alert(Error(response.statusText + " To API " + Endpoint));
+    .then(Response => {
+      if (!Response.ok)   {
+        throw alert(Error(Response.statusText + " To API " + Endpoint));
       }
       else{
-        var jsonData = response.json();
-        return jsonData;
+        var JsonData = Response.json();
+        return JsonData;
       }
     })
-    .then(jsonData => {
-      cleanupJson(jsonData);
+    .then(JsonData => {
+      cleanupJson(JsonData);
     })
   } catch (Err) {
     console.error(Err);
@@ -33,6 +34,7 @@ function getFillLevels() {
 
 // Enevo supplier
 function getEnevoFillLevels() {
+    // Api filter properties handling for enevo
   let DateFilter = '/?&time=2020-02-08T15%3A03%3A50Z';
   const EnevoEndpoint = '/enevo/filllevels';
   apiCall(EnevoEndpoint, DateFilter);
@@ -40,37 +42,39 @@ function getEnevoFillLevels() {
 
 // Sidcom supplier
 function getSidconFillLevels() {
+  // Api filter properties handling for sidcon
   let DateFilter = '/?detailed=1&communication_date_time=2020-02-08T11%3A00%3A03.210000Z';
   const SidconEndpoint = '/sidcon/filllevels';
   apiCall(SidconEndpoint, DateFilter);
 }
 
-function cleanupJson(jsonData)
+function cleanupJson(JsonData)
 {
-  console.log("jsonData", jsonData);
-  var cleanResults = jsonData.results;
-  cleanResults = cleanResults.slice(0, 2);
-  //Filter the coordinates to array<<<<============
-  // var geometrie = cleanResults.results.geometrie.coordinates;
-  console.log(cleanResults.results.geometrie.coordinates);
-  var lookup = {};
-  var result = [];
+  console.log("jsonData", JsonData);
+  var CleanResults = JsonData.results;
+  // CleanResults = CleanResults.slice(0, 2);
+
+  var Lookup = {};
+  var Result = [];
   // Only distinct records. (Reason: api response includes alot of duplicate values)
-  for (var item, i = 0; item = cleanResults[i++];) {
-    var name
-    item.e_site_name == null? name = item.container_id : name = item.e_site_name;
+  for (var Item, i = 0; Item = CleanResults[i++];) {
+    var Name;
+    Item.e_site_name == null? name = Item.container_id : name = Item.e_site_name;
   // If name doesnt exist in lookup then add to array and add the name in lookup for the check
-    if (!(name in lookup)) {
-      lookup[name] = 1;
-      result.push(item);
+    if (!(Name in Lookup)) {
+      Lookup[Name] = 1;
+      Result.push(Item);
     }
   }
-    createGarbageContainers(result);
+    createGarbageContainers(Result);
 }
 
 // Create garbage container elements
 function createGarbageContainers(cleanResult) {
   cleanResult.forEach(element => {
+    var fill_level = element.fill_level == undefined ? element.filling : element.fill_level;
+    var location = element.e_site_name == undefined ? element.geometrie.coordinates : element.e_site_name
+    let garbageContainer = new GarbageContainer(fill_level, location);
 
     let garbageSection = document.getElementById('GarbageContainers');
     let containerElement = document.createElement("div");
@@ -78,16 +82,13 @@ function createGarbageContainers(cleanResult) {
     let fillLvlTextElem = document.createElement("p");
     let locationTextElement = document.createElement("p");
 
-
     containerElement.setAttribute('class', 'garbage-container');
     textContainerElement.setAttribute('class', 'garbage-text-container');
     fillLvlTextElem.setAttribute('class', 'garbage-text');
     locationTextElement.setAttribute('class', 'garbage-text');
     
-    var fillLevel = element.fill_level == undefined ? element.filling : element.fill_level;
-
-    fillLvlTextElem.textContent = "Fill Level: " + fillLevel;
-    locationTextElement.textContent = "Location: " + element.e_site_name;
+    fillLvlTextElem.textContent = "Fill Level: " + garbageContainer.fill_level;
+    locationTextElement.textContent = "Location: " + garbageContainer.formatLocation();
 
     garbageSection.appendChild(containerElement);
     containerElement.appendChild(textContainerElement);
